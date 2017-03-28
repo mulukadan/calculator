e4bbe693e89eafc2bbad648c5c755cec1c9abb29
mulukadan <kadan.md@gmail.com>
2017-03-28 16:26:00 +0300

    Adding calculator files

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var add = function(num1, num2){
+     return num1 + num2;
+};
+num1 = parseInt(prompt("Enter Num 1"));
+num2 = parseInt(prompt("Enter Num 2"));
+alert(num1 +" + "+ num2 +" = " + add(num1,num2));
